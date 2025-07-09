9f738e75578a0733a206fd134e10d87049947cc1
Vinicius Marotti <vinicius.marotti@hotmail.com>
2025-07-09 14:56:25 -0300

    setup backend server

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import { fastifyCors } from '@fastify/cors';
+import { fastify } from 'fastify';
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod';
+// import {} from './db/connection.ts';
+import { env } from './env.ts';
+import { getRoomsRoute } from './http/routes/get-rooms.ts';
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+  origin: 'http://localhost:5173',
+});
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.register(getRoomsRoute);
+
+app.get('/health', () => {
+  return 'Ok';
+});
+
+app.listen({ port: env.PORT });
